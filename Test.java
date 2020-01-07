eb91eef0a9f171d5bae365aef117174049db4c27
Rohit Jain <rohitjain.arya2@gmail.com>
2020-01-08 01:02:08 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,52 @@
+package helloworld;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.yaml.snakeyaml.Yaml;
+
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+        Yaml yaml = new Yaml();
+        InputStream in = Files.newInputStream(Paths.get("C:\\Users\\rohit\\Desktop\\test.yml"));
+
+            TreeMap<String, Map<String, Object>> config = yaml.loadAs(in, TreeMap.class);
+            System.out.println(String.format("%s%n\nConverts to Properties:%n%n%s", config.toString(), toProperties(config)));
+        
+    }
+
+    private static String toProperties(TreeMap<String, Map<String, Object>> config) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String key : config.keySet()) {
+
+            sb.append(toString(key, config.get(key)));
+        }
+
+        return sb.toString();
+    }
+
+    private static String toString(String key, Map<String, Object> map) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String mapKey : map.keySet()) {
+
+            if (map.get(mapKey) instanceof Map) {
+                sb.append(toString(String.format("%s.%s", key, mapKey), (Map<String, Object>) map.get(mapKey)));
+            } else {
+                sb.append(String.format("%s.%s=%s%n", key, mapKey, map.get(mapKey).toString()));
+            }
+        }
+
+        return sb.toString();
+    }
+}
